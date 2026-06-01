887d8069c300c048cc6f97db237c0e18c7161ce1
danielphilip12 <daniel.gallo12@gmail.com>
2026-06-01 15:54:15 -0700

    Start state analysis

--- sql/views.sql
+++ sql/views.sql
@@ -12,3 +12,27 @@ from weather_observation wo
 join location l
     on wo.location_id = l.id
 group by city, state, observed_at::date;
+
+create view full_table as
+select
+    wo.id as weather_observation_id,
+    wo.location_id,
+    wo.observed_at,
+    wo.temperature,
+    wo.feels_like,
+    wo.humidity,
+    wo.pressure,
+    wo.wind_speed,
+    wo.weather_description,
+    wo.weather_code,
+    wo.raw_json,
+    wo.created_at as weather_observation_created_at,
+    l.city,
+    l.state,
+    l.country,
+    l.latitude,
+    l.longitude,
+    l.created_at as location_created_at
+from weather_observation wo
+join location l
+    on wo.location_id = l.id;
